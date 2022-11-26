d319b8be942465160890af05fb40d47c98267fd2
LucioFider <fidelislucio404@gmail.com>
2022-11-27 08:50:55 +1000

    2nd-commit

--- sections/World.jsx
+++ sections/World.jsx
@@ -1,9 +1,10 @@
-'use client';
+"use client";
 
-const World = () => (
-  <section>
-    World section
-  </section>
-);
+import { motion } from "framer-motion";
+import styles from "../styles";
+import { staggerContainer, fadeIn } from "../utils/motion";
+import { TitleText, TypingText } from "../components";
+
+const World = () => <section>World section</section>;
 
 export default World;
